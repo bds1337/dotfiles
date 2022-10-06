5cb3b8b362bfd438d3852d312e14a5ebe145cd39
Danila <cauze4concern@yandex.com>
2022-10-06 16:02:55 +0700

    Update install-plug.sh

--- .config/nvim/install-plug.sh
+++ .config/nvim/install-plug.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "rust-analysis shit installing"
+rustup component add rls rust-analysis rust-src
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
